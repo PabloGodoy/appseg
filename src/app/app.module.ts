1ce6773c955a2b889e08510310f5afed39b41831
Pablo Godoy <pgodoy@MacBook-Pro-de-Pablo.local>
2017-09-20 09:04:43 -0300

    cambio de repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
-
+import { HttpModule } from '@angular/http';
+import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -26,6 +30,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     SpeechRecognition,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { SpeechRecognition } from '@ionic-native/speech-recognition';
 import {  NgZone } from '@angular/core';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -11,8 +14,48 @@ export class HomePage {
 
   isListening: boolean = false;
   matches: Array<String>;
+  muestra: any;
+  busca: boolean;
+  data: any = {};
+  palabra: any;
+  encontrado: any = "no";
+  texto: any;
 
-  constructor(public navCtrl: NavController, public speech: SpeechRecognition, private zone: NgZone) {
+  constructor(private storage: Storage, public toastCtrl: ToastController, public geolocation: Geolocation, public navCtrl: NavController, public speech: SpeechRecognition, public http: Http, private zone: NgZone) {
+  this.data.nombre = '';
+  this.data.telefono = '';
+  this.data.contacto = '';
+  this.data.fonocontacto = '';
+  this.data.lat = '';
+  this.data.lon = '';
+  this.data.response = '';
+  this.http = http;
+  }
+
+  submit() {
+    var link = 'http://cognitivachile.com/appsegdev/api.php';
+    var myData = JSON.stringify({lat: this.data.lat, lon: this.data.lon, nombre: this.data.nombre, telefono: this.data.telefono, contacto: this.data.contacto, fonocontacto: this.data.fonocontacto, texto: this.texto});
+    
+    this.http.post(link, myData).subscribe(data => {
+      this.data.response = data["_body"]; 
+    }, error => {
+      console.log("algo esta mal jejeje!");
+    });
+  }
+
+  localizar() {
+    this.geolocation.getCurrentPosition({timeout: 6000})
+                .then( info => {
+                  this.data.lat = info.coords.latitude;
+                  this.data.lon = info.coords.longitude;
+                })
+                .catch(error =>{
+                  let toast = this.toastCtrl.create({
+                    message: 'No se pudo encontrar la ubicacion',
+                    duration: 2000
+                  });
+                  toast.present();
+                })
   }
 
   async hasPermission():Promise<boolean> {
@@ -35,7 +78,8 @@ export class HomePage {
   }
 
   listen(): void {
-    console.log('listen action triggered');
+    console.log('Escuchando');
+    this.localizar();
     if (this.isListening) {
       this.speech.stopListening();
       this.toggleListenMode();
@@ -49,14 +93,27 @@ export class HomePage {
       .subscribe(matches => {
         _this.zone.run(() => {
           _this.matches = matches;
+          _this.encontrado = "no";
+          for (var i = 0; i < matches.length; i++) { 
+            var re = new RegExp(_this.palabra,"gi"); 
+            if (matches[i].search(re) == -1 ) { 
+              _this.muestra = "no hay hotword";
+            } else { 
+              _this.texto = matches[i];
+              _this.encontrado = "si";
+            } 
+          }
+          if (_this.encontrado == "si") {
+            _this.muestra = "Hotword encontrada";
+            _this.submit();
+          }
         })
       }, error => console.error(error));
-
   }
 
   toggleListenMode():void {
     this.isListening = this.isListening ? false : true;
-    console.log('listening mode is now : ' + this.isListening);
+    console.log('te estoy escuchando XD : ' + this.isListening);
   }
 
 }
